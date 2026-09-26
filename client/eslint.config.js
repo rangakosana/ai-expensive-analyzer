a3f1f475f462fc0a5a38a2d6d60ca768722e30a1
Ranga <rangakosana29@gmail.com>
2026-09-26 12:43:33 +0530

    build: enforce react-hooks rules-of-hooks during build to prevent runtime hook errors

---
+++ client/eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        localStorage: 'readonly',
+        fetch: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+        console: 'readonly',
+        alert: 'readonly',
+        CustomEvent: 'readonly',
+        Event: 'readonly',
+        AbortController: 'readonly',
+        FileReader: 'readonly',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+    },
+  },
+];
